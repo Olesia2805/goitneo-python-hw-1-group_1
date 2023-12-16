86a83c0ae60436fcc9d27baee09b8ade798096c4
Olesia2805 <vysotina2002@gmail.com>
2023-12-16 23:23:48 +0100

    HW2

--- Bot_Helper.py
+++ Bot_Helper.py
@@ -1,66 +1,80 @@
-"""The function takes a string of user input, breaks it into words.
+def input_error(func):
+    def inner(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except (ValueError, KeyError, IndexError) as e:
+            return str(e)
+
+    return inner
+
+def parse_input(user_input):
+    """The function takes a string of user input, breaks it into words.
 It returns the first word as the command cmd and the rest as a list of arguments *args.
 Next, it removes extra spaces around the command and converts it to lower case."""
-def parse_input(user_input):
     command, *args = user_input.split()
     command = command.strip().lower()
     return command, *args
 
-"""this add-function has to have 2 args (name and phone (int number))
-which it will use for adding to the list"""
+@input_error
 def add_contact(args, contacts):
-    if len(args) == 2:
-        name, phone = args
-        if phone.isdigit():
-            contacts[name] = phone
-            return "Contact added."
-        else:
-            return "Second argument need to be phone number"
-    else:
-        return "Please try again. You should write name AND phone"
+    """this add-function has to have 2 args (name and phone (int number))
+which it will use for adding to the list"""
+    
+    name, phone = args
+    
+    if phone is not phone.isdigit() and len(phone) < 10:
+        raise ValueError ("Second argument need to be phone number")
+    
+    contacts[name] = phone
+    return "Contact added."
 
-"""this change-function has to have 2 args (name and phone (int number))
-which it will use for changing the user's phone to the list"""
+@input_error
 def change_contact(args, contacts):
-    if len(args) == 2:
-        name, phone = args
-        if name in contacts:
-            if phone.isdigit():
-                contacts[name] = phone
-                return "Contact updated."
-            else:
-                return "Second argument need to be phone number"
-        else:
-            return f"I don't find {name} in list"   
-    else:
-        return "Please try again. You should write name AND phone on which change it"
-
-"""this command show someone's phone number (if it is possible)"""
+    """this change-function has to have 2 args (name and phone (int number))
+which it will use for changing the user's phone to the list"""
+    if len(args) < 2:
+        raise ValueError("Please provide both name and phone.")
+    
+    name, phone = args
+    
+    if name not in contacts:
+        raise ValueError ("Second argument need to be phone number")
+    
+    if phone is not phone.isdigit():
+        raise KeyError (f"I don't find {name} in list")            
+    
+    contacts[name] = phone
+    return "Contact updated."
+        
+                          
+@input_error
 def show_phone(args, contacts):
+    """this function show someone's phone number (if it is possible)"""
     if len(args) == 1:
         name = args[0]
         if name in contacts:
             return contacts[name]
         else:
-            return f"I don't find {name} in list"
+            raise ValueError (f"I don't find {name} in list")
     else:
-        return "Please try again. You should write ONLY name"
+        raise IndexError ("Please try again. You should write ONLY name")
 
-"""show all contacts which has in the phone book"""
+@input_error
 def show_all(contacts):
+    """show-all function show all contacts which has in the phone book"""
     if contacts:
         for name, phone in contacts.items():
             return "\n".join([f"{name}: {phone}"])
     else:
-        return "No contacts available."
+        raise ValueError ("No contacts available.")
 
-"""main function where everything happening"""
 def main():
+    """main function where everything happening"""
     contacts = {}
     print("\n💫 Welcome to the assistant bot!🌟")
 
-    """this cycle will work until user not write command "close or exit"""
     while True:
+        """this cycle will work until user not write command "close or exit"""
         print("""
         WRITE COMMAND:
             For example:
@@ -73,9 +87,9 @@ def main():
         user_input = input("\nEnter a command:")
         command, *args = parse_input(user_input)
         
-        """these commands are letters' independed and for 
-        each command call function which is written in the top"""
         if command in ["close", "exit"]:
+            """these commands are letters' independed and for 
+        each command call function which is written in the top"""
             print("Good bye!")
             break
         elif command == "hello":
